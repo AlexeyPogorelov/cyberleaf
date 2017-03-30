bf19a0f3fe9e8d37516e61d7e53aba4636224c4c
AlexeyPogorelov <hirurg555@gmail.com>
2017-03-31 01:28:35 +0300

    blog

--- src/js/background.js
+++ src/js/background.js
@@ -15,16 +15,21 @@ Background.prototype.renderFrame = function () {
 	// this.frames++;
 	this.updateState();
 
+	this.ctx.clearRect(0, 0, this.width, this.height);
+
 	// demo
 	breakpoints.getCurrent();
 
 	var progress = breakpoints.getProgress();
-	progress = Math.floor(progress * 255).toString(16);
-	if (progress.length === 1) {
-		progress = '0' + progress;
-	}
-	this.radialGradient(progress + progress + progress, progress + progress + progress);
-}
+	// progress = Math.floor(progress * 255).toString(16);
+	// if (progress.length === 1) {
+	// 	progress = '0' + progress;
+	// }
+
+	// this.radialGradient(progress + progress + progress, progress + progress + progress);
+	this.ctx.font="60px Verdana";
+	this.ctx.fillText( Math.floor(progress * 100) ,200, 200, 400 );
+};
 
 Background.prototype.radialGradient = function (from, to) {
 	var gradient = this.ctx.createRadialGradient(
@@ -39,17 +44,17 @@ Background.prototype.radialGradient = function (from, to) {
 	gradient.addColorStop(1, "#" + to);
 	this.ctx.fillStyle = gradient;
 	this.ctx.fillRect(0, 0, this.width, this.height);
-}
+};
 
 Background.prototype.updateState = function () {
 	scrollTop = window.pageYOffset;
 	breakpoints.update(scrollTop, windowHeight);
-}
+};
 
 Background.prototype.rnd = function () { // @tmrDevelops : just alternative for Math.random();
 	Math.seed = (Math.seed * 108013 + 2531011) & 0xffffffff;
 	return Math.abs(Math.seed >> 16) / 32869;
-}
+};
 
 
 
@@ -58,16 +63,16 @@ Background.prototype.rnd = function () { // @tmrDevelops : just alternative for 
 
 setTimeout(function () {
 
-var background = new Background(
+	var background = new Background(
 		document.getElementById("hero-canvas")
-	)
+	);
 
-setTimeout(function () {
-(function loop() {
-	background.renderFrame();
-	window.requestAnimationFrame(loop);
-})();
-}, 200);
+	setTimeout(function () {
+		(function loop() {
+			background.renderFrame();
+			window.requestAnimationFrame(loop);
+		})();
+	}, 200);
 
 }, 200);
 
